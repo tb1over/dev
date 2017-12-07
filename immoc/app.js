a92761ddb823b47eac3008e241a2b7d402653957
tb1over <nxsfcc@gmail.com>
2017-12-08 00:07:31 +0800

    update something

--- immoc/app.js
+++ immoc/app.js
@@ -16,12 +16,57 @@ console.log(`server running at port:${port}`);
 
 //路由
 app.get('/', (req, res) =>{
-    res.render('index', {title:'imovie 首页'});
+    res.render('index', {
+        title:'imovie 首页',
+        movies:[{
+            title: '战狼2',
+            _id:1,
+            doctor: '吴京',
+            country: '中国',
+            year:2017,
+            poster: 'https://puui.qpic.cn/vcover_vt_pic/0/wi8e2p5kirdaf3j1501228536/260',
+            language: '中文',
+            flash: '',
+            summary: '吴京导演战狼2'
+        },{
+            title: '战狼2',
+            _id:1,
+            doctor: '吴京',
+            country: '中国',
+            year:2017,
+            poster: 'https://puui.qpic.cn/vcover_vt_pic/0/wi8e2p5kirdaf3j1501228536/260',
+            language: '中文',
+            flash: '',
+            summary: '吴京导演战狼2'
+        },{
+            title: '战狼2',
+            _id:1,
+            doctor: '吴京',
+            country: '中国',
+            year:2017,
+            poster: 'https://puui.qpic.cn/vcover_vt_pic/0/wi8e2p5kirdaf3j1501228536/260',
+            language: '中文',
+            flash: '',
+            summary: '吴京导演战狼2'
+        }]
+    });
 });
 
 //admin
 app.get('/admin/movie', (req, res) =>{
-    res.render('admin', {title:'imovie 后台页面'});
+    res.render('admin', {
+        title:'imovie 后台页面',
+        movie: {
+            title: '',
+            doctor: '',
+            country: '',
+            year: '',
+            poster: '',
+            summary: '',
+            flash: '',
+            language: ''
+        }
+    });
 });
 
 //list
